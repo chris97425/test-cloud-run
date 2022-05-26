e19d5f2b860665e2e264b49dfeb9959c13ab56b3
christopher de boisvilliers <christopher.deboisvilliers@gmail.com>
2022-05-26 15:44:42 +0200

    add awesome stuff

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.8-buster
 COPY api.py api.py
-RUN pip install -U pip
-RUN pip install fastapi uvicorn
+RUN pip3 install -U pip
+RUN pip3 install fastapi uvicorn nltk
+RUN python -m nltk.downloader vader_lexicon
 CMD uvicorn api:app --host 0.0.0.0 --port $PORT
--- api.py
+++ api.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = FastAPI()
 
 
-@app.get("/")
+
+@app.get("/", response_class=HTMLResponse)
 def root():
-    return "Hello from Cloud Run CD"
+    return  f"""
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+        <h1> Predict sentiment with a sentence </h1>
+         <form action="/vader-predict" method="get">
+        <label for="fname">Texte a analyser</label>
+        <input type="text" id="sentence" name="sentence"><br><br>
+
+        <input type="submit" value="Submit">
+        </form>
+
+        </body>
+
+    </html>
+    """
+
+@app.get("/vader-predict")
+def predict_vader(sentence):
+
+    sid = SentimentIntensityAnalyzer()
+    ss = sid.polarity_scores(sentence)
+    ss["sentence"]= sentence
+    return ss
